26a4186130fde5766d64a5c55490abf2e782ac57
Keane Dwight Sulit <keane_sulit@dlsu.edu.ph>
2023-04-06 20:43:46 +0800

    Added mikroC files

---
+++ mikroC/COCIMIC_FP_Compiler.c
@@ -0,0 +1,12 @@
+/*
+This is a firmware code for the project, "A Simple Temperature-Controlled Fan using PIC16F877A".
+*/
+
+void init() {
+    // Initialize LCD library
+    Lcd_Init();
+}
+
+void main() {
+
+}
